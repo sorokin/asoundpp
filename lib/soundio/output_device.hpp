0c650313587280ab057aa1a825ab371b7fe72936
Ivan Sorokin <sorokin@rain.ifmo.ru>
2012-10-28 19:23:05 +0400

    minor: clarify comment

--- lib/soundio/output_device.hpp
+++ lib/soundio/output_device.hpp
@@ -10,7 +10,7 @@ struct output_device : boost::noncopyable
     output_device(std::string const& device_name, format fmt);
 
     // block execution if buffer of operating system is full
-    // buf must contain at least fmt.frame_size() * size bytes
+    // buf must contain at least fmt.frame_size() * number_of_frames bytes
     void write(void const* buf,
                size_t number_of_frames);
 
